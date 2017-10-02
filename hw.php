94143a79ec3537915498dd784454be43ac77bcc8
Peter J Jough <pjj5@afsconnect2.njit.edu>
2017-10-01 21:28:09 -0400

    change for number 4-6

--- hw.php
+++ hw.php
@@ -1,10 +1,12 @@
 <?php
 
-$date =  date('Y/m/d', time());
+$date = date('Y/m/d', time());
 echo "The value of \$date: ".$date."<br>";
 
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
+
+/////////////////////////////////////////////////
 
 if ($date > $tar && $date+$tar >0){
 	echo "The future<br>";
@@ -15,6 +17,19 @@ else if ($date+$tar == 0){
 else{
 	echo "The past<br>";
 }
+
+/////////////////////////////////////////////////
+echo "The positions are...<br>";
+echo strpos($date,"/")." ";
+echo strrpos($date,"/")."<br>";
+
+/////////////////////////////////////////////////
+
+echo "The word count for \$date is ".str_word_count($date)."<br>";
+
+echo "The string length of \$date is ".strlen($date)."<br>";
+
+/////////////////////////////////////////////////
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
